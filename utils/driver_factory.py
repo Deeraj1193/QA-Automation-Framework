c548eae2138f3a4029ad0aef9539d87e59578ec1
Deeraj N <mail4deeraj@gmail.com>
2026-03-05 14:57:30 +0530

    Local file version mismatch bug fix

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,28 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class DriverFactory:
 
     @staticmethod
-    def create_driver(headless=True):
+    def create_driver(headless=False):
 
-        chrome_options = Options()
+        options = Options()
 
         if headless:
-            chrome_options.add_argument("--headless=new")
+            options.add_argument("--headless=new")
 
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--window-size=1920,1080")
+        options.add_argument("--start-maximized")
 
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
-            options=chrome_options
-        )
+        # FORCE correct ChromeDriver version
+        service = Service(ChromeDriverManager(driver_version="145").install())
 
-        driver.implicitly_wait(5)
+        driver = webdriver.Chrome(service=service, options=options)
 
         return driver
